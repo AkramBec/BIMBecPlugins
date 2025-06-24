904189dfd073aa98b0a38d9851e49662a60cbd97
AkramBec <akram.bec.96@gmail.com>
2025-06-25 02:26:59 +0300

    Initial commit

---
+++ source/SearchFittingsInParkingSpaces/Application.cs
@@ -0,0 +1,28 @@
+﻿using JetBrains.Annotations;
+using Nice3point.Revit.Extensions;
+using Nice3point.Revit.Toolkit.External;
+using SearchFittingsInParkingSpaces.Commands;
+
+namespace SearchFittingsInParkingSpaces
+{
+    /// <summary>
+    ///     Application entry point
+    /// </summary>
+    [UsedImplicitly]
+    public class Application : ExternalApplication
+    {
+        public override void OnStartup()
+        {
+            CreateRibbon();
+        }
+
+        private void CreateRibbon()
+        {
+            var panel = Application.CreatePanel("Машиноместо", "ТЗ Бешшар");
+
+            panel.AddPushButton<StartupCommand>("Поиск арматуры")
+                .SetImage("/SearchFittingsInParkingSpaces;component/Resources/Icons/RibbonIcon16.png")
+                .SetLargeImage("/SearchFittingsInParkingSpaces;component/Resources/Icons/RibbonIcon32.png");
+        }
+    }
+}
---
+++ source/SearchFittingsInParkingSpaces/Models/ResultInfo.cs
@@ -0,0 +1,51 @@
+﻿namespace SearchFittingsInParkingSpaces.Models;
+
+public class ResultInfo
+{
+    private readonly Element _element;
+    private readonly TitleInfo _titleInfo;
+    
+    public int ElementId {get;}
+    public string Category {get;}
+    public string SystemClassification {get;}
+    public string Name {get;}
+    public string Designation {get;}
+    public string DocumentTitle {get;}
+    public string Discipline { get; }
+    public string ViewName { get;}
+    public ElementId ViewId { get;}
+    
+    public ResultInfo(Element element, string documentTitle, ElementId viewId)
+    {
+        _element = element;
+        DocumentTitle = documentTitle;
+        ViewId = viewId;
+
+        ElementId = element.Id.IntegerValue;
+        Category = element.Category?.Name ?? "Нет категории";
+        SystemClassification = element.get_Parameter(BuiltInParameter.RBS_SYSTEM_NAME_PARAM).AsString();
+
+        if (documentTitle != "")
+        {
+            Discipline = TitleInfo.Discipline(documentTitle);
+            ViewName = $"{Discipline}_{Category}_{ElementId}";
+        }
+        else
+        {
+            ViewName = $"{Category}_{ElementId}";
+        }
+    }
+
+    public override string ToString()
+    {
+        if (DocumentTitle != "")
+        {
+            return $"{Discipline} : {ElementId}, Категория: {Category}";
+        }
+        else
+        {
+            return $"{ElementId}, Категория: {Category}";
+        }
+        
+    }
+}
